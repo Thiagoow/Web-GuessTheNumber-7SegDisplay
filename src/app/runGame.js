456242fe18e7ba3576cbc57eb2222f26a11f3a23
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-08-28 21:28:17 -0300

    Greater, less or equal script added

--- src/app/runGame.js
+++ src/app/runGame.js
@@ -1,10 +1,25 @@
+import { domElements } from './domElements.js'
+const { sendBtn, sendValue } = domElements
+sendBtn.addEventListener('click', startGame)
+
 import getRandomNumber from './fetchAPI.js'
+const randomNumber = await getRandomNumber()
 import { setErrorValue } from './setDisplay.js'
 
-const randomNumber = await getRandomNumber()
+function startGame() {
+  if (randomNumber > sendValue.value) {
+    alert('É maior')
+  } else if (randomNumber < sendValue.value) {
+    alert('É menor')
+  } else {
+    alert('YOU WIN!')
+  }
+}
 
 if (typeof randomNumber === 'object') {
   setErrorValue(randomNumber.StatusCode)
 } else {
   console.log(randomNumber)
 }
+
+//Maior ou menor em inglÊs:
